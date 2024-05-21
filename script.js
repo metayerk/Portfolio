fcc882f7c1c7a6624ef3aa75557ee0c04becef04
NokeOrgrim <108398847+NokeOrgrim@users.noreply.github.com>
2024-05-21 21:06:26 +0200

    Update nav bar for PC

--- script.js
+++ script.js
@@ -4,13 +4,47 @@ const complet_name = name.getAttribute("value")
 function motTyping(word, index, lenght){
     if(index < lenght){
         setTimeout(() => {
-            console.log(name)
             name.innerHTML += word[index]
             motTyping(word, index + 1, lenght)
-        },250)
+        },350)
     }
 }
 
 setTimeout(() => {
+    console.log(name)
     motTyping(complet_name, 0, 10)
-}, 500)
+})
+
+const boutonNav = document.getElementById('onOff')
+const nav = document.getElementById('nav')
+const navReduit = document.getElementById('off')
+const navAgrandit = document.getElementById('on')
+let switchNav = 0;
+
+boutonNav.onclick = (() => {
+    console.log(switchNav)
+    if (switchNav == 1){
+        switchNav = 0
+    }
+    else{
+        switchNav = 1
+    }
+    reactNav(switchNav)
+})
+
+
+function reactNav(flag){
+    if(flag === 1){
+        console.log('on')
+        navReduit.style.display = "none"
+        navAgrandit.style.display = "block"
+        nav.style.width = "500px"
+    }
+    else{
+        console.log('off')
+        navReduit.style.display = "block"
+        navAgrandit.style.display = "none"
+        nav.style.width = "100px"
+    }
+
+}
