66c4e50c1b9e6439c8849ec474f5053afa4266ab
NokeOrgrim <108398847+NokeOrgrim@users.noreply.github.com>
2024-05-27 15:22:45 +0200

    Update 27/05/24
    Ajout de l'avenir + modif sur le style

--- script.js
+++ script.js
@@ -19,32 +19,55 @@ const boutonNav = document.getElementById('onOff')
 const nav = document.getElementById('nav')
 const navReduit = document.getElementById('off')
 const navAgrandit = document.getElementById('on')
-let switchNav = 0;
+let switchNav = 1;
 
 boutonNav.onclick = (() => {
+
     console.log(switchNav)
     if (switchNav == 1){
         switchNav = 0
     }
+
     else{
         switchNav = 1
     }
-    reactNav(switchNav)
-})
 
+    reactNav(switchNav)
+}
+)
+
+reactNav(switchNav)
+window.onresize = (() => {reactNav(switchNav)})
 
 function reactNav(flag){
+
     if(flag === 1){
         console.log('on')
         navReduit.style.display = "none"
         navAgrandit.style.display = "block"
-        nav.style.width = "500px"
+        boutonNav.innerHTML = "&#8656"
+        boutonNav.style.fontSize = "xx-large"
+        if(innerWidth > 800){
+            nav.style.width = "1200px"}
+        else if(innerWidth < 8000){
+            nav.style.width = "2400px"
+        }
+        else{
+            nav.style.width = "700px"
+        }
     }
+
     else{
         console.log('off')
         navReduit.style.display = "block"
         navAgrandit.style.display = "none"
-        nav.style.width = "100px"
+        boutonNav.innerHTML = "&#8658"
+        boutonNav.style.fontSize = "large"
+        if(innerWidth < 1200){
+            nav.style.width = "450px"
+        }else{
+        nav.style.width = "200px"
+        }
     }
 
 }
