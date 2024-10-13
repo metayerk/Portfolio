5f08d62c423a56e9124afbbaf5af4675bf4517d1
NokeOrgrim <108398847+NokeOrgrim@users.noreply.github.com>
2024-10-13 17:07:27 +0200

    Rework
    Version 2.0, Portfolio.
    4 pages dans un seul fichier html.
    version claire et sombre.
    Restructuration des menus contextuelle.
    Nouvelle police.
    Nouvelle charte graphique.

--- script.js
+++ script.js
@@ -1,101 +1,130 @@
-const name = document.getElementById('name')
-const complet_name = name.getAttribute("value")
 
-function motTyping(word, index, lenght){
-    if(index < lenght){
-        setTimeout(() => {
-            name.innerHTML += word[index]
-            motTyping(word, index + 1, lenght)
-        },250)
-    }
+/*THEME*/
+const themeToggleBtn = document.getElementById('theme-toggle');
+let theme = false;
+
+themeToggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('light_theme');
+  
+  if (document.body.classList.contains('light_theme')) {
+    theme=true;
+    themeToggleBtn.style.color = "#FF5722";
+  } else {
+    theme = false;
+    themeToggleBtn.style.color = "black";
+  }
+  changecolor(null);
+});
+
+/*CHANGEMENT DE PAGE*/
+
+let menu = document.getElementById("menu_page");
+let realisations = document.getElementById("realisations_page");
+let projets = document.getElementById("competence_page");
+let parcours = document.getElementById("parcours_page");
+
+let nav = document.getElementById("nav");
+let nav_real = document.getElementById("nav_real");
+let nav_proj = document.getElementById("nav_comp");
+let nav_parc = document.getElementById("nav_parc");
+let nav_acceuil = document.getElementById("nav_acceuil");
+let id_select;
+
+let id_list = [menu,realisations,projets,parcours];
+let nav_list = [nav_real, nav_parc, nav_proj]
+
+document.getElementById("nav_acceuil").onclick = ()=>{
+    changepage(menu); 
+    nav.style.display = "none";
+}
+document.getElementById("realisations").onclick = ()=>{
+    changepage(realisations);
+    changecolor(nav_real);
+}
+document.getElementById("competence").onclick = ()=>{
+    changepage(projets);
+    changecolor(nav_proj);
+}
+document.getElementById("parcours").onclick = ()=>{
+    changepage(parcours);
+    changecolor(nav_parc);
 }
 
-    console.log(name)
-    motTyping(complet_name, 0, 14)
-
-
-const boutonNav = document.getElementById('onOff')
-const nav = document.getElementById('nav')
-const navReduit = document.getElementById('off')
-const navAgrandit = document.getElementById('on')
-let switchNav = 1;
-
-boutonNav.onclick = (() => {
-
-    console.log(switchNav)
-    if (switchNav == 1){
-        switchNav = 0
-    }
-
-    else{
-        switchNav = 1
-    }
-
-    reactNav(switchNav)
+nav_real.onclick = ()=>{
+    changepage(realisations);
+    changecolor(nav_real);
+    
 }
-)
-
-reactNav(switchNav)
-window.onresize = (() => {reactNav(switchNav)})
-
-function reactNav(flag){
-
-    if(flag === 1){
-        console.log('on')
-        navReduit.style.display = "none"
-        navAgrandit.style.display = "block"
-        boutonNav.innerHTML = "&#8656"
-        boutonNav.style.fontSize = "xx-large"
-        if(innerWidth < 800 ){
-            nav.style.width = "2000px"}
-        else{
-            nav.style.width = "900px"
-        }
-    }
-
-    else{
-        console.log('off')
-        navReduit.style.display = "block"
-        navAgrandit.style.display = "none"
-        boutonNav.innerHTML = "&#8658"
-        boutonNav.style.fontSize = "large"
-        if(innerWidth < 1200){
-            nav.style.width = "450px"
+nav_proj.onclick = ()=>{
+    changepage(projets);
+    changecolor(nav_proj);
+}
+nav_parc.onclick = ()=>{
+    changepage(parcours);
+    changecolor(nav_parc);
+}
+function changecolor(id){
+    for (let i = 0; i<nav_list.length; i++){
+        if(theme==false){
+            nav_list[i].style.border = "#CCCCCC 3px dashed";
         }else{
-        nav.style.width = "200px"
+            nav_list[i].style.border = "#000000 3px dashed";
         }
     }
+    if(id != null){
+        id.style.border = "#FF7043 3px dashed";
+        id_select = id;
+    }
+    console.log(id);
 
 }
 
-const boutonOff = document.getElementById("exit");
+
+function changepage(id){
+for (let i = 0; i<id_list.length; i++){
+    id_list[i].style.display = "none";
+}
+    nav.style.display = "flex";
+    id.style.display = "flex";
+    
+    console.log(id);
+    
+}
+
+/* MENU CONTEXTUELLE */
 const interface = document.getElementById("interface");
 const body = document.getElementById("body");
-const navbar = document.getElementById("nav");
+const boutonOff = document.getElementById("exit");
 let page;
 
 function activeWindow(id){
 
-    interface.style.display = "flex"
-    body.style.overflow = "hidden"
-    navbar.style.display = "none"
-    console.log(id)
-    activePage(id) 
+    interface.style.display = "flex";
+    body.style.overflow = "hidden";
+    console.log(id);
+    activePage(id);
 
 }
 
-function activePage (id){
-    page = document.getElementById(id)
-    page.style.display = "block"
+function activePage (id){ // A LANCER SUR LE BOUTON
+    page = document.getElementById(id);
+    page.style.display = "flex";
+    if(theme==false){
+        id_select.style.border = "#CCCCCC 3px dashed";
+    }else{
+        id_select.style.border = "#000000 3px dashed";
+    }
 }
 
-function disableWindow (){
+function disableWindow(){
     disabledPage();
-    interface.style.display = "none"
-    navbar.style.display = "flex"
+    interface.style.display = "none";
 }
 
 function disabledPage (){
-    page.style.display = "none"
-    body.style.overflow = "auto"
+    id_select.style.border = "#FF7043 3px dashed";
+    page.style.display = "none";
+    body.style.overflow = "auto";
+    console.log(page);
 }
+
